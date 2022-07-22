86dbd36c0ba3a452bde627f60ce651fa57f6bb48
Nino van Hooff <nino@q42.nl>
2022-07-22 16:28:03 +0200

    muted wip

--- Source/main.lua
+++ Source/main.lua
@@ -1,13 +1,19 @@
 import "midi"
+import "ViewModel"
+import "View"
 
 local gfx = playdate.graphics
+
+gfx.setFont(playdate.graphics.font.new("fonts/font-pedallica"))
 
 local sequence = loadMidi('europe_finalcountdown_60s.mid')
 sequence:play()
 
-gfx.setColor(gfx.kColorBlack)
+local viewModel = ViewModel(sequence)
+local view = View(viewModel)
 
 function playdate.update()
-    gfx.fillRect(0, 0, 400, 240)
-    playdate.drawFPS(0,0)
+    viewModel:update()
+    view:draw()
+    playdate.timer:updateTimers()
 end
